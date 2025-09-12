56c956a82122260f44369ec81904fff5a8fa8e99
PadarZsombi <zsomborpadar0@gmail.com>
2025-09-12 12:58:55 +0200

    Ciklus2

---
+++ ciklus.py
@@ -0,0 +1,51 @@
+#ciklusok
+
+# lista = [1, 2, 3, 4, 5]
+
+
+# def osszegez(lista):
+#     osszeg = 0
+#     for elem in lista:
+#         osszeg += elem
+#     return osszeg
+
+# def paros_szamok_darabszama(lista):
+#     db = 0
+#     for szam in lista:
+#         if szam % 2 == 0:
+#             db+=1
+#     return db
+
+
+# print(f"A lista elemeinek összege: {osszegez(lista)}")
+
+# print(f"A lista páros elemeinek darabszáma: {paros_szamok_darabszama(lista)}")
+
+# # Írass ki egy sorba 5 *-ot
+# print("*"*5)
+
+# for i in range(5):
+#     print("*", end="")
+# print()
+
+# Rajzolj piramist
+#     *
+#    ***
+#   ***** 
+#  *******
+# *********
+
+n = 9
+space_db = n - 1
+csillag_db = 1
+for i in range(n):
+    print(" " * space_db, end="")
+    print("*" * csillag_db)
+    space_db -= 1
+    csillag_db += 2
+
+
+for i in range(1, 11):
+    for j in [1,2,3,4,5,6,7,8,9,10]:
+        print(f"{i*j:4}", end = "")
+    print()
---
+++ ciklus2.py
@@ -0,0 +1,11 @@
+# Kérjünk be számokat és adjuk meg az összegüket
+# 1 0 végjelig
+#2 Enter végjelig HF:ugyan ez az algoritmus enter végjelig
+osszeg = 0
+be_n=1
+while be_n !=0:
+
+    be_n=int(input("Kérek egy számot: "))
+    osszeg+=be_n
+
+print(osszeg)
