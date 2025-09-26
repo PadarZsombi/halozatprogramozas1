e9c1f6a1453f18bbf322c10969522909866e5777
PadarZsombi <zsomborpadar0@gmail.com>
2025-09-26 12:13:23 +0200

    Oraijavito

--- oraimunk.py
+++ oraimunk.py
@@ -3,10 +3,12 @@ szavak = nev.split()
 monogram = ""
 
 for szo in szavak:
-    if szo.lower().startswith("dzs"):
-        monogram += "DZS"
-    elif szo.lower().startswith(("cs","gy","ly","ny","sz","ty","zs")):
-        monogram += szo[:2].upper()
+    szo_lower = szo.lower()
+    
+    if szo_lower.startswith("dzs"):
+        monogram += "Dzs"
+    elif szo_lower.startswith(("cs", "gy", "ly", "ny", "sz", "ty", "zs")):
+        monogram += szo[:2].capitalize()
     else:
         monogram += szo[0].upper()
 
