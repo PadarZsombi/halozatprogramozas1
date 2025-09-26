412d5978d9ec55d6e46a610619161ec5e23a5be3
PadarZsombi <zsomborpadar0@gmail.com>
2025-09-26 12:00:46 +0200

    oraimunka

---
+++ oraimunk.py
@@ -0,0 +1,13 @@
+nev = input("Add meg a teljes neved: ")
+szavak = nev.split()
+monogram = ""
+
+for szo in szavak:
+    if szo.lower().startswith("dzs"):
+        monogram += "DZS"
+    elif szo.lower().startswith(("cs","gy","ly","ny","sz","ty","zs")):
+        monogram += szo[:2].upper()
+    else:
+        monogram += szo[0].upper()
+
+print("Monogram:", monogram)
